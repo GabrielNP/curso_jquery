c17d092079261516c1421770ebb9ac96a338d374
gabrielnp <gabrielnp.2010@gmail.com>
2019-10-13 20:32:12 -0300

    escolher frases parte 1

--- alura-typer/public/js/frases.js
+++ alura-typer/public/js/frases.js
@@ -1,11 +1,19 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
+    $("#spinner").toggle();
     $.get("http://localhost:3000/frases", trocaFrase)
     .fail(function(){
         $(".erro").toggle();
         setTimeout(function(){
             $(".erro").toggle();
+        },3000);
+    })
+    .always(function(){
+        // $("#spinner").toggle();
+        setTimeout(function(){
+            $("#spinner").toggle();
         },3000);
     });
     ;
@@ -17,4 +25,29 @@ function trocaFrase(data){
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId };
+
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $(".erro").toggle();
+        setTimeout(function(){
+            $(".erro").toggle();
+        },3000);
+    })
+    .always(function(){
+        // $("#spinner").toggle();
+        setTimeout(function(){
+            $("#spinner").toggle();
+        },3000);
+    });
+    ;
+}
+
+function trocaFrase(data){
+
 }
